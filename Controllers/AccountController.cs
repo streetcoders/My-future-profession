2ec51800e3fdf128c99f89374453e0a433068224
lizza00 <48031271+lizza00@users.noreply.github.com>
2019-04-23 19:24:28 +0300

    Add files via upload
    не знаю будет ли работать
    у меня куча пакетов
    которые я даже в архиве не могу кинуть

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserContext db = new UserContext();
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                User user = null;
+                user = await db.GetUsersAsync(model.Email, pass: model.Password);
+
+
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, true);
+                    return RedirectToAction("About", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "There is no such user");
+                }
+            }
+
+            return View(model);
+        }
+        public ActionResult Register()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async System.Threading.Tasks.Task<ActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = null;
+                //user = db.Users.Find(new FilterDefinitionBuilder<User>().Regex("Email", new BsonRegularExpression(model.Email))).ToList()[0];
+                Task<User> t = null;
+                user= await db.GetUserE(model.Email);
+                if (user==null)
+                {
+                    await db.Create(new User { Email = model.Name, Password = model.Password, Name = model.Name });
+                    FormsAuthentication.SetAuthCookie(model.Email, true);
+                    return RedirectToAction("Index", "Home");
+                    
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User with this login is existing");
+                }
+            }
+            return View(model);
+        }
+        [Authorize]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Models/Models.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string Number { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 7, ErrorMessage = "New passwords must be a minimum of 7 characters, please try a different password.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password should contain at least 6 symbols.")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        
+        [DataType(DataType.ImageUrl)]
+        public string ImageId { get; set; }
+
+
+    }
+}
