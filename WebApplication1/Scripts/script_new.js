0cb9868b1d0bf67489928301b07ff93ba9afb950
shhhook <shhhook04@gmail.com>
2019-04-28 14:47:06 +0300

    Fix
    -

--- WebApplication1/Scripts/script_new.js
+++ WebApplication1/Scripts/script_new.js
@@ -1,7 +1,7 @@
 var checkerAnim = false;
     checkerProfile = false;
     width = screen.width;
-    profile_t;
+    profile_t = 0;
 function animatingMove(which,x,y){
     which.style.top = x;
     which.style.left = y;
@@ -11,6 +11,7 @@ $(document).ready(function(){
         $(".keep").toggleClass("hide-by-disp");
         $(".contacts").toggleClass("hide-by-disp");
     });
+    
     $("article").click(function(){
         if ($(".keep").hasClass("hide-by-disp") == false){
             $(".keep").toggleClass("hide-by-disp");
@@ -41,13 +42,13 @@ $(document).ready(function(){
         $(".keep").toggleClass("width");
         if (!checkerProfile) {
            profile_t = setTimeout(function(){
-            $(".profile").toggleClass("hide-by-disp");
-            $(".profile-small").toggleClass("hide-by-disp");
+            $(".profile").removeClass("hide-by-disp");
+            $(".profile-small").addClass("hide-by-disp");
         },200); 
        }else{
        		clearTimeout(profile_t);
-            $(".profile").toggleClass("hide-by-disp");
-            $(".profile-small").toggleClass("hide-by-disp");
+            $(".profile").addClass("hide-by-disp");
+            $(".profile-small").removeClass("hide-by-disp");
        }
         
         checkerProfile = !checkerProfile;
